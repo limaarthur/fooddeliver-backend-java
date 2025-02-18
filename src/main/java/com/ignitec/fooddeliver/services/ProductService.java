cfcbca5d298403a30b751ea07581f6770bbb5846
limaarthur <artglima.dev@gmail.com>
2025-02-18 09:31:37 -0300

    feat: creation endpoints - creation ProductDTO and ProductService

---
+++ src/main/java/com/ignitec/fooddeliver/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.ignitec.fooddeliver.services;
+
+import com.ignitec.fooddeliver.dtos.ProductDTO;
+import com.ignitec.fooddeliver.entities.Product;
+import com.ignitec.fooddeliver.repositories.ProductRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public List<ProductDTO> findAll() {
+        List<Product> productList = productRepository.findAll();
+        List<ProductDTO> productListDTO = productList.stream().map(product -> new ProductDTO(product)).toList();
+        return productListDTO;
+    }
+}
